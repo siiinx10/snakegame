320077aead4f410dacc96b489a95edcc485d757c
Manarsh Nowbut <91077872+siiinx10@users.noreply.github.com>
2021-11-29 16:49:08 +0400

    Create board.java

---
+++ board.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Snake extends JFrame{
+
+    Snake(){
+        super("Snake Game");
+        add(new Board());
+        pack();
+        
+        setLocationRelativeTo(null);
+//        setTitle("Snake Game");
+        setResizable(false);
+      
+    }
+    
+    public static void main(String[] args){
+        new Snake().setVisible(true);
+    }
+}
